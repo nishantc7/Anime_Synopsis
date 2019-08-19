6a3aaa0e694d3d01aa847b8c47dd6bf91b6dcf79
Nishant Choudhary <35103134+nishantc7@users.noreply.github.com>
2019-08-20 01:16:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,71 @@
+from tkinter import *
 from bs4 import BeautifulSoup
 import requests
 import lxml
 import re
 
-#Name of the anime to be searched
-name_anime=input("Enter the name of the anime to search\n")
-name_update=name_anime.replace (" ","%20")
+#main window
+window = Tk()
+window.title("Anime Synopsis \n")
+window.configure(background="black")
 
-#requesting main
-
-search_string_1="https://myanimelist.net/search/all?q="
-final_search=search_string_1+name_update
-#print(final_search)
-search_init=requests.get(final_search)
-search_soup=BeautifulSoup(search_init.text,'lxml')
-the_final_link=""
-for link in search_soup.find_all('a', attrs={'class' : 'hoverinfo_trigger fw-b fl-l'},limit=1):
-
-	#print(link['href'])
-	the_final_link=link['href']
-	#print(the_final_link)
-
-
-
-def scrapeit():
+#scraping function
+def scrapeit(name_anime):
 	#ff
+	name_update=name_anime.replace (" ","%20")
+	search_string_1="https://myanimelist.net/search/all?q="
+	final_search=search_string_1+name_update
+	#print(final_search)
+	search_init=requests.get(final_search)
+	search_soup=BeautifulSoup(search_init.text,'lxml')
+	the_final_link=""
+	for link in search_soup.find_all('a', attrs={'class' : 'hoverinfo_trigger fw-b fl-l'},limit=1):
+
+		#print(link['href'])
+		the_final_link=link['href']
+		#print(the_final_link)
 	res=requests.get(the_final_link)
 #res_content=res.content
 	soup=BeautifulSoup(res.text,"lxml")
 	soup_re=soup.find_all("span", itemprop="description")
 #print(soup.title.string)
 	for i in soup_re:
-		print(i.text)
+		return(i.text)
 
-scrapeit()
+#click_button
+def click():
+	entered_text=textentry.get()
+	output.delete(0.0, END)
+	defination=scrapeit(entered_text)
+	output.insert(END,defination)
+
+
+#background 
+picture_1=PhotoImage(file="./idk_cool_flower.gif")
+Label(window, image=picture_1, bg="black").grid(row=0,column=0,sticky=W)
+
+
+#label1
+Label(window, text="For which anime would you like to find the Synopsis?\n", bg="black", fg="white", font="none 12 bold" ).grid(row=1,column=0,sticky=W)
+
+#entry_box_name
+textentry = Entry(window, width=20, bg="white")
+textentry.grid(row=2, column=0, sticky=W)
+
+#submit button
+Button(window, text="Submit", width=6, command=click).grid(row=3, column=0, sticky=W)
+
+#label2
+Label(window,text="Synopsis:\n", bg="black", fg="white", font="none 12 bold").grid(row=4, column=0, sticky=W)
+
+#output text
+output=Text(window,width=100, height=30, wrap=WORD, background="white")
+output.grid(row=5, column=0, columnspan=3, sticky=W)
+
+
+
+#exit function
+
+
+#mainloop
+window.mainloop()
