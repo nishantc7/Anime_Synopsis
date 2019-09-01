ba3a7f2b7fc79a97a5cc53dbec76be7b58dc311c
Nishant Choudhary <35103134+nishantc7@users.noreply.github.com>
2019-09-01 16:24:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,34 @@
 from tkinter import *
-from bs4 import BeautifulSoup
-import requests
-import lxml
-import re
-import tkinter.scrolledtext as tkscrolled
-
-#main window
-window = Tk()
-window.title("Anime Synopsis \n")
-window.configure(background="black")
-window.geometry("1280x720")
-
-#scraping function
-def scrapeit(name_anime):
-	#ff
-	name_update=name_anime.replace (" ","%20")
-	search_string_1="https://myanimelist.net/search/all?q="
-	final_search=search_string_1+name_update
-	#print(final_search)
-	search_init=requests.get(final_search)
-	search_soup=BeautifulSoup(search_init.text,'lxml')
-	the_final_link=""
-	for link in search_soup.find_all('a', attrs={'class' : 'hoverinfo_trigger fw-b fl-l'},limit=1):
-
-		#print(link['href'])
-		the_final_link=link['href']
-		#print(the_final_link)
-	res=requests.get(the_final_link)
-#res_content=res.content
-	soup=BeautifulSoup(res.text,"lxml")
-	soup_re=soup.find_all("span", itemprop="description")
-#print(soup.title.string)
-	for i in soup_re:
-		return(i.text)
-
-#click_button
-def click():
-	entered_text=textentry.get()
-	output.delete(0.0, END)
-	try:
-		defination=scrapeit(entered_text)
-	except:
-		defination="Do you even watch Anime?"
-	output.insert(END,defination)
+from additional_file import *
+the_name="hero"
 
 
-#background 
-picture_1=PhotoImage(file="./idk_cool_flower.gif")
-Label(window, image=picture_1, bg="black").grid(row=2,column=0,sticky=N+S+E+W)
+def when_called():
 
+	def input_function():
 
-#label1
-Label(window, text="For which anime would you like to find the Synopsis?\n", bg="black", fg="white", font="none 12 bold" ).grid(row=3,column=0,sticky=N+S+E+W)
+		the_name_update=click2()
+		the_function(the_name_update)
+	
+	def click2():
+		entered_text=textentry.get()
+		#the_name=entered_text
+		input_window.destroy()
+		return entered_text
+			
+	input_window = Tk()
+	input_window.title("Anime Synopsis")
+	input_window.configure(background="black")
 
+	#label1
+	Label(input_window, text="For which anime would you like to find the Synopsis?\n", bg="black", fg="white", font="none 12 bold" ).grid(row=3,column=0,sticky=N+S+E+W)
+	#entry_box_name
+	textentry = Entry(input_window, width=20, bg="white")
+	textentry.grid(row=4, column=0,sticky=N+S+E+W)
+	#submit button
+	Button(input_window, text="Submit", width=6, command=input_function).grid(row=5, column=0,sticky=W)
 
-#entry_box_name
-textentry = Entry(window, width=20, bg="white")
-textentry.grid(row=4, column=0,sticky=N+S+E+W)
+	#mainloop for input window
+	input_window.mainloop()
 
-
-#submit button
-Button(window, text="Submit", width=6, command=click).grid(row=5, column=0,sticky=W)
-
-
-#label2
-Label(window,text="Synopsis:\n", bg="black", fg="white", font="none 12 bold").grid(row=6, column=0,sticky=N+S+E+W)
-
-
-#output text
-output=tkscrolled.ScrolledText(window,width=100, height=6, wrap=WORD, background="white")
-output.grid(row=7, column=0, columnspan=3,sticky=N+S+E+W)
-
-#exit function
-def close_window():
-	window.destroy()
-	exit()
-
-
-
-#exit label
-Label(window,text="Click here to exit\n", bg="black", fg="white", font="none 12 bold").grid(row=0, column=0,sticky=N+S+E+W)
-
-#exit button
-Button(window,text="Exit", width=14, command=close_window).grid(row=1, column=0,sticky=N+S+E+W)
-
-
-#mainloop
-window.mainloop()
+when_called()
